97a57b34b3bfbe4b2d9bab9cb12589c41dec4721
Cosmin <cosminstefanxp@gmail.com>
2012-04-03 22:15:10 +0300

    first commit

---
+++ src/ml/scenario/ScenarioEvent.java
@@ -0,0 +1,16 @@
+package ml.scenario;
+
+/**
+ * The Class ScenarioEvent.
+ */
+public class ScenarioEvent {
+	
+	/** The time of the event. */
+	public int time;
+	
+	/** The start floor of the passenger. */
+	public int startFloor;
+	
+	/** The destination floor of the passenger. */
+	public int stopFloor;
+}
---
+++ src/ml/scenario/ScenarioGenerator.java
@@ -0,0 +1,5 @@
+package ml.scenario;
+
+public class ScenarioGenerator {
+
+}
